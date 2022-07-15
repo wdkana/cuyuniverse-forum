50e727a3f659be559e5f94e445cb6f5d6a8e267d
deaaprizal <dea.afrizal@icloud.com>
2022-07-15 13:41:19 +0700

    whitelist

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -21,7 +21,8 @@ class Cors
         $browsers = ['Mozilla', 'Firefox', 'Chrome', 'Edge', 'Safari'];
         $isBrowser = false;
         $whitelist = ['localhost', 'http://174.138.20.8', 'http://cuyuniverse.co', 'http://www.cuyuniverse.co', 'https://cuyuniverse.co', 'https://www.cuyuniverse.co', 'http://137.184.248.42', '137.184.248.42', '174.138.20.8', '*.cuyuniverse.co', 'http://*.cuyuniverse.co', 'https://*.cuyuniverse.co'];
-        $normal = '*';
+        $noWhiteList = '*';
+        $cred = true;
 
         foreach ($browsers as $browser) {
             if (strpos($userAgent, $browser) !==  false) {
@@ -31,10 +32,10 @@ class Cors
         }
         if ($isBrowser === true) {
             return $next($request)
-                ->header('Access-Control-Allow-Origin', $normal)
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Origin', $whitelist)
+                ->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
                 ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type')
-                ->header('Access-Control-Allow-Credentials', true)
+                ->header('Access-Control-Allow-Credentials', $cred)
                 ->header('max_age', 120);
         }
         return $next(abort(403, ''));
