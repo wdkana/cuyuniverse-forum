c1b8f2b83391c2e913cc9bfcfd29ccfe753d915a
deaaprizal <dea.afrizal@icloud.com>
2022-07-19 11:41:30 +0700

    add page handling

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<title>Halaman Tidak Ditemukan</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>Halaman Tidak Ditemukan</h1>
+    <div>
+        <p>Mohon maaf halaman yang kamu cari saat ini tidak ada. <a href="/">Kembali ke halaman utama</a></p>
+    </div>
+</article>
