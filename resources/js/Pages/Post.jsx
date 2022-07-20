9ae0bfee02df89aade8629435ff9eb8f7eb5c987
deaaprizal <dea.afrizal@icloud.com>
2022-07-20 15:42:43 +0700

    fix card width

--- resources/js/Pages/Post.jsx
+++ resources/js/Pages/Post.jsx
@@ -12,7 +12,7 @@ export default function PostPage(props) {
         <div className='text-center pt-6'>
           <h1 className='font-bold text-lg'>✨ {props.title} ✨</h1>
         </div>
-        <div className='flex flex-col justify-center items-center lg:flex-row lg:flex-wrap lg:items-stretch pt-6 px-4 gap-6'>
+        <div className='flex flex-col justify-center items-center lg:flex-row lg:flex-wrap lg:items-stretch pt-6 px-2 lg:px-4 gap-6'>
           <PostList posts={props.data[0]} users={props.auth.user} notif={props.flash.message} />
         </div>
       </div>
