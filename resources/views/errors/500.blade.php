7286db6d4d630663db70d9ec9180bac333f319b2
deaaprizal <dea.afrizal@icloud.com>
2022-07-20 15:25:21 +0700

    add throtling

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>Cuy Universe Off Duls ✌</h1>
+    <div>
+        <p>Mohon maaf saat ini website sedang dalam <i>pengecekan</i> guna memperbaiki performa untuk kepuasan Cuy People. Kalau kamu pengen bertanya sesuatu <a href="mailto:deacode@icloud.com">hubungi admin sekarang</a>, terimakasih CuyUniverse akan kembali online dari beberapa waktu kedepan!</p>
+        <p>&mdash; Dea Afrizal</p>
+    </div>
+</article>
