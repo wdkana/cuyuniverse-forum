6dab2b9c61687cd287c813bdeb9698e82671868d
farhanreizha <reizha226@gmail.com>
2022-07-23 22:01:59 +0700

    alert responsive

--- resources/js/Components/Default/NotificationAlert.jsx
+++ resources/js/Components/Default/NotificationAlert.jsx
@@ -6,7 +6,7 @@ const isNotif = (message) => {
   if (message) {
     return (
       <div className="toast toast-top toast-center z-10">
-        <div className="alert bg-sky-500 text-white w-96">
+        <div className="alert bg-sky-500 text-white w-52 sm:w-72">
           <span className="text-sm">{message}</span>
         </div>
       </div>
