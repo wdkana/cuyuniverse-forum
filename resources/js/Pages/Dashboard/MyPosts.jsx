ba17b79ccf3cdf24d2136962acbddcc14a241e8a
faruqmaulana <faruqchodot21@gmail.com>
2022-07-24 00:59:14 +0700

    feat: add saved post

--- resources/js/Pages/Dashboard/MyPosts.jsx
+++ resources/js/Pages/Dashboard/MyPosts.jsx
@@ -33,7 +33,6 @@ export default function MyPosts(props) {
         return setWantRemove(false);
     };
 
-<<<<<<< HEAD
     return (
         <Authenticated
             auth={props.auth}
@@ -51,52 +50,6 @@ export default function MyPosts(props) {
                     >
                         {props.next}
                     </Link>
-=======
-  return (
-    <Authenticated
-      auth={props.auth}
-      errors={props.errors}
-      header={
-        <div className='flex flex-row justify-between'>
-          <h2 className="font-semibold text-xl leading-tight cursor-default">{props.page}</h2>
-          <Link href={`${route(props.nextRoute)}`} method="get" as="button" className="btn btn-sm btn-ghost leading-tight">{props.next}</Link>
-        </div>
-      }
-    >
-      <Head title="Dashboard" />
-      <div className='flex flex-col justify-center items-center lg:flex-row lg:flex-wrap lg:items-strech pt-6 px-4 gap-6'>
-        {showNotif && <NotificationAlert message={flash.message} />}
-        {props.data.length > 0 ? props.data.map((posts, i) => {
-          return (
-            <div key={i} className="card w-full md:w-1/2 lg:w-1/3 bg-base-100 shadow-lg">
-              <div className="card-body ">
-                <Link href={`/post/${posts.id}`} method="get" as="div" className='card-title'>
-                  {
-                    posts.image !== null ? <img src={`/storage/images_post/${posts.image}`} className="object-cover h-48 w-100 rounded-lg" alt=""></img> : null
-                  }
-                  <p className={`cursor-pointer hover:-translate-y-1 hover:transition-all text-xl text-left ${posts.description.length > 100 ? "break-normal overflow-x-hidden" : "break-words"} h-20`}>{posts.description}</p>
-                </Link>
-                <div className="card-actions justify-between">
-                  <div className='text-xs'>
-                    posted {formatTime(posts.updated_at)} | {posts.comments.length} comment
-                  </div>
-                  <label onClick={() => handleRemoveConfirmation()} className="btn btn-ghost cursor-pointer hover:bg-base-300 hover:rounded-md modal-button" htmlFor={`my-modal-${posts.id}`}><TbTrashX size={20} /></label>
-                  {wantRemove &&
-                    <>
-                      <input type="checkbox" id={`my-modal-${posts.id}`} className="modal-toggle" />
-                      <div className="modal">
-                        <div className="modal-box">
-                          <h3 className="font-bold text-lg">Hapus Postingan </h3>
-                          <p className="py-4">Kamu yakin ingin menghapus postingan <b>{posts.title}</b>?</p>
-                          <div className="modal-action">
-                            <label htmlFor={`my-modal-${posts.id}`} className="btn btn-outline rounded-md" onClick={() => removePosts(posts.id)}>Ya Hapus</label>
-                            <label htmlFor={`my-modal-${posts.id}`} className="btn btn-inline rounded-md">Gak</label>
-                          </div>
-                        </div>
-                      </div>
-                    </>
-                  }
->>>>>>> main
                 </div>
             }
         >
