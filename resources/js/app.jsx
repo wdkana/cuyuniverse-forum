8f3d4a26a1f03ad47d94c3f8d8f18ad7b3bba492
= <=>
2022-07-24 00:30:48 +0700

    merapikan route dan beberapa code

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -11,11 +11,7 @@ const appName = window.document.getElementsByTagName("title")[0]?.innerText;
 
 createInertiaApp({
   title: (title) => `${appName} - ${title}`,
-  resolve: (name) =>
-    resolvePageComponent(
-      `./Pages/${name}.jsx`,
-      import.meta.glob("./Pages/**/*.jsx")
-    ),
+  resolve: (name) => resolvePageComponent( `./Pages/${name}.jsx`, import.meta.glob("./Pages/**/*.jsx") ),
   setup({ el, App, props }) {
     return render(<App {...props} />, el);
   },
