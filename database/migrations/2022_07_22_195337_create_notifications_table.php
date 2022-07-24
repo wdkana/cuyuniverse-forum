902e40898da345d2a98448c597b993c73839f896
deaaprizal <dea.afrizal@icloud.com>
2022-07-24 17:06:38 +0700

    fix id on notif migration

--- database/migrations/2022_07_22_195337_create_notifications_table.php
+++ database/migrations/2022_07_22_195337_create_notifications_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->uuid('id')->primary();
             $table->string('type');
             $table->morphs('notifiable');
