2c42fd94ac46d0962c36edc29d712da8076e6aaf
farhanreizha <reizha226@gmail.com>
2022-07-24 23:17:37 +0700

    fix toggle dark mode and hover lihat postingan

--- resources/js/Components/Homepage/DarkToggle.jsx
+++ resources/js/Components/Homepage/DarkToggle.jsx
@@ -15,11 +15,7 @@ const DarkToggle = () => {
       }
     });
 
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
+    if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
       document.documentElement.classList.add("dark");
     } else {
       document.documentElement.classList.remove("dark");
@@ -29,16 +25,12 @@ const DarkToggle = () => {
   return (
     <div className="relative">
       <div className="absolute">
-        <input
-          id="theme-toggle"
-          type="checkbox"
-          className="focus:outline-none rounded-md text-sm p-5 opacity-0 z-[99999999999999]"
-        />
+        <input id="theme-toggle" type="checkbox" className="focus:outline-none rounded-md text-sm p-5 opacity-0 z-[99999999999999]" />
       </div>
-      <div className="p-1.5 rounded-md focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 bg-slate-200 dark:bg-slate-700 shadow-md shadow-slate-400 dark:shadow-slate-900">
+      <div className="p-1.5 rounded-md focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 bg-slate-700 dark:bg-slate-200 shadow-md shadow-slate-400 dark:shadow-slate-900">
         <svg
           id="theme-toggle-dark-icon"
-          className="w-7 h-7 "
+          className="w-7 h-7 text-white dark:text-black"
           fill="currentColor"
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
