f25fdddd0f31568e409d0cb03cbbbae05510f4f3
farhanreizha <reizha226@gmail.com>
2022-07-25 00:05:30 +0700

    fix toggle dark mode and change password input color

--- resources/js/Pages/Dashboard/ChangePassword.jsx
+++ resources/js/Pages/Dashboard/ChangePassword.jsx
@@ -1,75 +1,79 @@
-import React, { useEffect, useState } from 'react';
-import Authenticated from '@/Layouts/Authenticated';
-import { Head, Link, usePage } from '@inertiajs/inertia-react';
-import { useForm } from '@inertiajs/inertia-react'
-import NotificationAlert from '@/Components/Default/NotificationAlert';
+import React, { useEffect, useState } from "react";
+import Authenticated from "@/Layouts/Authenticated";
+import { Head, Link, usePage } from "@inertiajs/inertia-react";
+import { useForm } from "@inertiajs/inertia-react";
+import NotificationAlert from "@/Components/Default/NotificationAlert";
 
 export default function ChangePassword(props) {
   const { data, setData, put, processing, reset } = useForm({
-    oldPassword: '',
-    newPassword: '',
-    newPassword_confirmation: '',
+    oldPassword: "",
+    newPassword: "",
+    newPassword_confirmation: "",
     token: props.auth.user.token,
   });
 
   const [confirmUpdate, setConfirmUpdate] = useState(false);
-  const [showNotif, setShowNotif] = useState(false)
-  const { flash } = usePage().props
+  const [showNotif, setShowNotif] = useState(false);
+  const { flash } = usePage().props;
 
   useEffect(() => {
     if (flash.message) {
-      setShowNotif(true)
+      setShowNotif(true);
     }
 
     return () => {
-      reset('oldPassword', 'newPassword', 'newPassword_confirmation')
-      setConfirmUpdate(false)
-      setShowNotif(false)
-    }
-  }, [props])
+      reset("oldPassword", "newPassword", "newPassword_confirmation");
+      setConfirmUpdate(false);
+      setShowNotif(false);
+    };
+  }, [props]);
 
   function handleChange(e) {
-    const name = e.target.getAttribute('name')
+    const name = e.target.getAttribute("name");
 
-    if (name === 'confirm') {
-      setConfirmUpdate(!confirmUpdate)
+    if (name === "confirm") {
+      setConfirmUpdate(!confirmUpdate);
     }
 
     setData({
       ...data,
       [name]: e.target.value,
-    })
+    });
   }
 
   function updatePassword(e) {
-    e.preventDefault()
-    console.log(data)
-    put(route('dash.update.password'))
+    e.preventDefault();
+    console.log(data);
+    put(route("dash.update.password"));
   }
 
   return (
     <Authenticated
       auth={props.auth}
       errors={props.errors}
-      header={<div className='flex flex-row justify-between'>
-        <h2 className="font-semibold text-xl leading-tight cursor-default">{props.title}</h2>
-        <Link href={route(`${props.nextRoute}`)} as="button" className="btn btn-sm btn-ghost rounded-md leading-tight">{props.next}</Link>
-      </div>}
+      header={
+        <div className="flex flex-row justify-between">
+          <h2 className="font-semibold text-xl leading-tight cursor-default">{props.title}</h2>
+          <Link href={route(`${props.nextRoute}`)} as="button" className="btn btn-sm btn-ghost rounded-md leading-tight">
+            {props.next}
+          </Link>
+        </div>
+      }
     >
       <Head title="Change Password" />
       {showNotif && <NotificationAlert message={flash.message} />}
       <div className="overflow-hidden sm:rounded-lg p-2 lg:p-10">
-        <div className='flex flex-col gap-6 items-center p-4 lg:gap-2'>
+        <div className="flex flex-col gap-6 items-center p-4 lg:gap-2">
           {props.errors && props.errors.oldPassword && <NotificationAlert message={props.errors.oldPassword} />}
           {props.errors && props.errors.newPassword && <NotificationAlert message={props.errors.newPassword} />}
-          <div className='flex justify-center w-full'>
+          <div className="flex justify-center w-full">
             <form onSubmit={updatePassword}>
               <div className="mb-2">
                 <label className="form-label font-semibold">Old Password</label>
                 <input
                   type="password"
                   name="oldPassword"
-                  className="w-full text-lg font-bold font-mono input input-bordered rounded-md"
+                  className="w-full text-lg text-black font-bold font-mono input input-bordered rounded-md"
                   required
                   value={data.oldPassword}
                   onChange={handleChange}
@@ -80,7 +84,7 @@ export default function ChangePassword(props) {
                 <input
                   type="password"
                   name="newPassword"
-                  className="w-full text-lg font-bold font-mono input input-bordered rounded-md"
+                  className="w-full text-lg text-black font-bold font-mono input input-bordered rounded-md"
                   required
                   value={data.newPassword}
                   onChange={handleChange}
@@ -92,13 +96,14 @@ export default function ChangePassword(props) {
                   type="password"
                   name="newPassword_confirmation"
                   required
-                  className="w-full text-lg font-bold font-mono input input-bordered rounded-md" value={data.newPassword_confirmation}
+                  className="w-full text-lg text-black font-bold font-mono input input-bordered rounded-md"
+                  value={data.newPassword_confirmation}
                   onChange={handleChange}
                 />
               </div>
               <div className="form-control">
                 <label className="label cursor-pointer">
-                  <span className="label-text">Confirm change password.</span> 
+                  <span className="label-text text-black dark:text-white">Confirm change password.</span>
                   <input
                     type="checkbox"
                     name="confirm"
@@ -109,7 +114,9 @@ export default function ChangePassword(props) {
                   />
                 </label>
               </div>
-              <button type='submit' disabled={processing} className='btn w-full border-0 rounded-md mt-2 bg-primary hover:bg-secondary'>Change</button>
+              <button type="submit" disabled={processing} className="btn w-full border-0 rounded-md mt-2 bg-primary hover:bg-secondary">
+                Change
+              </button>
             </form>
           </div>
         </div>
