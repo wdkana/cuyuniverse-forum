a5c63379775b61db13dbc411212bfb702df9f02f
Vino aryo <vinoaryo2000@gmail.com>
2022-07-25 23:49:08 +0700

    Fixing text color contrast in homepage/announcement

--- resources/js/Components/Homepage/Announcement.jsx
+++ resources/js/Components/Homepage/Announcement.jsx
@@ -6,10 +6,10 @@ const Announcemenet = () => {
       <pre data-prefix="1">
         <code>Versi BETA 1.1.0</code>
       </pre>
-      <pre data-prefix="2" className="bg-warning text-warning-content ">
+      <pre data-prefix="2" className="bg-warning text-warning-content">
         <code>sedang dalam development</code>
       </pre>
-      <pre data-prefix="3" className="bg-info">
+      <pre data-prefix="3" className="bg-info text-warning-content">
         <code><a href="https://github.com/deaaprizal/laract9" target="_blank">Source Code <BiLinkExternal className="inline" /></a></code>
       </pre>
     </div>
