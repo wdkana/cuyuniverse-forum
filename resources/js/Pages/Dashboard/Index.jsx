07ad5b1701444fc464474b1741ebe8c48f64f3a3
deaaprizal <dea.afrizal@icloud.com>
2022-07-26 10:36:04 +0700

    ui and routes redesign

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -3,6 +3,7 @@ import Authenticated from '@/Layouts/Authenticated';
 import { Head, Link, usePage } from '@inertiajs/inertia-react';
 import NotificationAlert from '@/Components/Default/NotificationAlert';
 import CreatePost from '@/Components/Dashboard/CreatePost';
+import { MdSettings } from 'react-icons/md';
 export default function DashboardPage(props) {
     const { flash } = usePage().props
     const [showNotif, setShowNotif] = useState(false)
@@ -18,10 +19,10 @@ export default function DashboardPage(props) {
         <Authenticated
             auth={props.auth}
             errors={props.errors}
-            header={<div className="font-semibold text-md dark:text-white">
-                Login as
-                <Link href={route('dash.setting.profile')} as="button" method="get" className='btn btn-ghost btn-sm underline -ml-2'>
-                    {props.auth.user.username}
+            header={<div className="font-semibold text-sm dark:text-white">
+                Your profile
+                <Link href={route('dash.setting.profile')} as="button" method="get" className='btn btn-ghost btn-sm'>
+                    ({props.auth.user.username} <MdSettings />)
                 </Link>
             </div>}
         >
