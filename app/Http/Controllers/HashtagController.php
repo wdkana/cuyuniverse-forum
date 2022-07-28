e536619d5b8bdf01b7e8f451a4c5ea4f4972f430
deaaprizal <dea.afrizal@icloud.com>
2022-07-29 01:47:34 +0700

    musverify active

---
+++ app/Http/Controllers/HashtagController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostsCollection;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class HashtagController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+}
