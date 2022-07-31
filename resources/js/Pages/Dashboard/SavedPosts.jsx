bc84cf408505e63f7d5d5cb5ea1536fb282a0734
faruqmaulana <faruqchodot21@gmail.com>
2022-07-31 21:06:21 +0700

    fix: responsive posts filter

--- resources/js/Pages/Dashboard/SavedPosts.jsx
+++ resources/js/Pages/Dashboard/SavedPosts.jsx
@@ -1,36 +1,33 @@
-import React from 'react';
-import { Head } from '@inertiajs/inertia-react';
-import Authenticated from '@/Layouts/Authenticated';
-import PostsList from '@/Components/Homepage/PostsLists';
+import React from "react";
+import {Head} from "@inertiajs/inertia-react";
+import Authenticated from "@/Layouts/Authenticated";
+import PostsList from "@/Components/Homepage/PostsLists";
 
 export default function SavedPost(props) {
   const data = props.data.map(value => {
     const comments = value.comments;
-    return { ...value.posts, comments }
-  })
+    return {...value.posts, comments};
+  });
 
   return (
-    <Authenticated
-      auth={props.auth}
-      errors={props.errors}
-    >
+    <Authenticated auth={props.auth} errors={props.errors}>
       <Head title={props.title} />
-      <div className='min-h-screen dark:text-white'>
+      <div className="min-h-screen dark:text-white">
         {data.length > 0 ? (
           <>
-            <div className='text-center pt-6'>
-              <h1 className='font-bold text-lg'>✨ {props.page} ✨</h1>
+            <div className="pt-6 text-center">
+              <h1 className="text-lg font-bold">✨ {props.page} ✨</h1>
             </div>
-            <div className='flex flex-col justify-center items-center lg:flex-row lg:flex-wrap lg:items-strech py-6 px-4 gap-6'>
+            <div className="lg:items-strech flex flex-col items-center justify-center gap-6 py-6 px-4 lg:flex-row lg:flex-wrap">
               <PostsList posts={data} />
             </div>
           </>
         ) : (
-          <div className='text-center pt-6'>
-            <h1 className='font-bold text-lg'>Belum ada postingan yang anda simpan 😥</h1>
+          <div className="pt-6 text-center">
+            <h1 className="text-lg font-bold">Belum ada postingan yang anda simpan 😥</h1>
           </div>
         )}
       </div>
     </Authenticated>
-  )
+  );
 }
