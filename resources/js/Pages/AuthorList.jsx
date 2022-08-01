242d1ccc03dde738f9a6478f1426e82a2aecb82c
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2022-08-01 22:44:26 +0700

    add condition for message handling empty data

--- resources/js/Pages/AuthorList.jsx
+++ resources/js/Pages/AuthorList.jsx
@@ -119,7 +119,7 @@ export default function AuthorListPage(props) {
                     strokeWidth="2"
                     d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
-                <span>Pengguna tidak ditemukan</span>
+                <span>{keyword ? 'Pengguna tidak ditemukan' : 'Data tidak ditemukan'}</span>
               </div>
             </div>
           </div>
