7bc69be08f173a0ea0fb61837215c859e9daceef
deaaprizal <dea.afrizal@icloud.com>
2022-08-04 13:26:16 +0700

    redesign home post

--- resources/js/Components/Homepage/PostsLists.jsx
+++ resources/js/Components/Homepage/PostsLists.jsx
@@ -1,5 +1,5 @@
-import {formatTime} from "@/utils/jsHelper";
-import {Link} from "@inertiajs/inertia-react";
+import { formatTime } from "@/utils/jsHelper";
+import { Link } from "@inertiajs/inertia-react";
 
 const noPosts = () => {
   return (
@@ -13,18 +13,25 @@ const noPosts = () => {
 
 const isPosts = (posts, from) => {
   return posts.map((post, i) => {
+    let more = false;
+    let desc = post.description
+    if (desc.length > 30) {
+      desc = desc.slice(0, 80)
+      more = true
+    }
     return (
       <div
         key={i}
-        className="card w-full cursor-pointer bg-base-100 shadow-lg transition-all duration-300 hover:-translate-y-1 hover:bg-base-200 dark:bg-slate-700 dark:text-white dark:hover:bg-gray-600 md:w-1/2 lg:w-1/3 xl:w-1/3">
-        <Link href={`/post/${post.id}`} method="get" as="div" className="card-body p-6">
-          <div className={`text-xl ${post.description.length > 80 ? "overflow-x-hidden pr-2" : "break-words"} h-20`}>
-            {post.description}
+        className="cursor-pointer md:border-2 md:p-2 bg-base-100 transition-all duration-300 hover:-translate-y-1 hover:bg-primary hover:text-primary-content dark:bg-slate-700 dark:text-white dark:hover:bg-gray-600 w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
+        <Link href={`/post/${post.id}`} method="get" as="div" className="flex flex-col p-4 justify-evenly">
+          <div className="text-md break-words h-20">
+            {desc}
+            {more && <span className="italic text-sm text-primary"> ...lebih lengkap</span>}
           </div>
 
           <div className="mt-2 flex flex-row items-center">
             <Link href={`/author/${post.author}`} as="button" method="get" className="avatar">
-              <div className="w-12 rounded-full ring ring-primary ring-offset-2 ring-offset-base-100">
+              <div className="w-6 rounded-full ring ring-primary ring-offset-2 ring-offset-base-100">
                 {from?.page == "author" ? (
                   <img
                     src={
@@ -45,7 +52,7 @@ const isPosts = (posts, from) => {
               </div>
             </Link>
             <div className="ml-4 text-sm">
-              <p className="leading-none text-gray-900 dark:text-white">{post.author}</p>
+              <p className="leading-none dark:text-white">{post.author}</p>
               <p className="break-normal text-xs">
                 posted {formatTime(post.updated_at)} |{" "}
                 {post.comments && post.comments.length > 0 ? post.comments.length : "no"} comment
