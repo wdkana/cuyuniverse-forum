b8aa804204a6adad2314ebe938490ea0453b6830
Muhamad Rafli <yusfiwawan2@gmail.com>
2022-08-05 21:32:16 +0700

    change post styles✨

--- resources/js/Components/Dashboard/CreatePost.jsx
+++ resources/js/Components/Dashboard/CreatePost.jsx
@@ -110,7 +110,12 @@ const CreatePost = ({ props }) => {
           {imagePreview &&
             <div className="image-full pb-4"><img src={imagePreview} /></div>
           }
-          <input className="p-4 w-full border border-spacing-1" type="file" name="image" accept="image/*" onChange={(e) => handleImagePost(e.target.files[0])} />
+          <input className="p-4  border border-spacing-1 block w-full text-sm text-slate-500
+      file:mr-4 file:py-2 file:px-4
+      file:rounded-full file:border-0
+      file:text-sm file:font-semibold
+      file:bg-violet-50 file:text-violet-700
+      hover:file:bg-violet-100" type="file" name="image" accept="image/*" onChange={(e) => handleImagePost(e.target.files[0])} />
         </div>
         <i>{textTagged[0]}</i>
         <div className="relative w-full rounded-lg border-4 p-2 lg:w-1/2">
