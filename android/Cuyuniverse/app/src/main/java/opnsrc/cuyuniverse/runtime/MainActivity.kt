3de0696d1a44c76f022a801028d31774fb5c575c
kafri8889 <kafri8889@gmail.com>
2022-08-18 20:15:50 +0700

    Aplikasi Cuyuniverse untuk android (native [Jetpack Compose])

---
+++ android/Cuyuniverse/app/src/main/java/opnsrc/cuyuniverse/runtime/MainActivity.kt
@@ -0,0 +1,28 @@
+package opnsrc.cuyuniverse.runtime
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import opnsrc.cuyuniverse.foundation.theme.CuyuniverseTheme
+
+class MainActivity : ComponentActivity() {
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			CuyuniverseTheme {
+				Surface(
+					modifier = Modifier.fillMaxSize(),
+					color = MaterialTheme.colorScheme.background
+				) {
+					// TODO: UI
+				}
+			}
+		}
+	}
+	
+}
