036658748923df11ae6f0ecbebd6279a5370454a
rizsyad <rizsyad@gmail.com>
2022-09-03 17:12:53 +0700

    fix navbar

--- resources/js/Components/Default/NavBar.jsx
+++ resources/js/Components/Default/NavBar.jsx
@@ -1,6 +1,11 @@
 import {HiMenuAlt2} from "react-icons/hi";
+import {useState} from "react";
 
-function NavBar() {
+const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const setToggleMenu = () => setShowMenu(!showMenu);
+
   return (
     <nav className="border-gray-200 bg-light-primary px-2 py-2.5 dark:bg-gray-900 sm:px-4">
       <div className="container mx-auto flex max-w-7xl flex-wrap items-center justify-between">
@@ -8,27 +13,13 @@ function NavBar() {
           <span className="self-center whitespace-nowrap text-4xl font-bold dark:text-white">Cuy!</span>
         </a>
         <button
-          data-collapse-toggle="navbar-default"
           type="button"
           className="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-          aria-controls="navbar-default"
-          aria-expanded="false">
+          onClick={() => setToggleMenu()}>
           <span className="sr-only">Open main menu</span>
-          <svg
-            className="h-6 w-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"></path>
-          </svg>
-
-          {/* <HiMenuAlt2 className="text-2xl" /> */}
+          <HiMenuAlt2 className="text-2xl" />
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div className={`${showMenu ? "hidden" : ""} w-full md:block md:w-auto`}>
           <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 p-4 dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:text-sm md:font-medium md:dark:bg-gray-900">
             <li>
               <a
@@ -71,6 +62,6 @@ function NavBar() {
       </div>
     </nav>
   );
-}
+};
 
 export default NavBar;
