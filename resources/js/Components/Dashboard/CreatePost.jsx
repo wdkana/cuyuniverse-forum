0bff47a61df6a9fbbbf534c34f9ccd7b763f878b
Kafaa Billahi Syahida <kafaabillaahisyahida135@gmail.com>
2022-11-24 13:34:13 +0700

    Revert "Remove console.log"
    This reverts commit 9a594ee140864d6b927ef493e46ff37b97fd23a0.

--- resources/js/Components/Dashboard/CreatePost.jsx
+++ resources/js/Components/Dashboard/CreatePost.jsx
@@ -15,6 +15,8 @@ const CreatePost = ({ props }) => {
   const [imageFile, setImageFile] = useState();
   const [imagePreview, setImagePreview] = useState("");
 
+  console.log(styles);
+
   const regexHastag = new RegExp(/(^|\W)(#[a-z\d][\w-]*)/ig);
 
   useEffect(() => {
